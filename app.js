cd2acc4bcd7f39aeb11ba45369d03718447790c2
Sun Lau <sun.lau@iheha.com>
2015-08-02 21:37:23 +0800

    first

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+ var restful = restful = require('node-restful');
+var session      = require('express-session');
+ var mongoose = restful.mongoose;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Allow CORS for Swagger
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use('/static', express.static('public'));
+
+
+app.use('/', routes);
+app.use('/api/v1/members', users);
+app.use('/api/v1/me', function(req,res,next){
+  console.log('body');
+  console.log(req.body);
+  console.log('query');
+  console.log(req.query);
+  console.log('session');
+  console.log(req.session.uid);
+  res.send('Hi!');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
