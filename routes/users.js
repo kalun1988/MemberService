e4bdf2ab4e4c5a08227378f885aad581114156a9
Sun Lau <sun.lau@iheha.com>
2015-08-02 21:36:24 +0800

    reset

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var libs = process.cwd()+'/';
-var db = require(libs + 'db/mongoose');
-var User = require(libs + 'models/user');
-
-User.register(router, '/');
-
-module.exports = router;
